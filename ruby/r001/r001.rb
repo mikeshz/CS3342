e97be226d27e2dcdc63358749e10dbad1dc6fbf7
Michael Song <michael.song.hz@gmail.com>
2017-01-29 16:02:42 -0500

    time r001 60 in progress

--- ruby/r001/r001.rb
+++ ruby/r001/r001.rb
@@ -2,29 +2,57 @@ def sayHi
    puts "Hello World!"
 end
 
-#puts ARGF.read
-# print all the lines in every file passed via command line that contains login
 inp = $stdin.read
 lineNumber = 0
 commitLine = ""
+singleCommit = ""
+outputString = ""
+type = ""
+
+jsCommits = Array.new
+rubyCommits = Array.new
+haskellCommits = Array.new
+miscCommits = Array.new
+
 inp.each_line do |line|
     #do something with line
     if lineNumber == 0 || line[0..5] == "commit"
-        commitLine = line;
+        outputString = singleCommit + outputString
+        if type == "j"
+            jsCommits.push(singleCommit)
+            #outputString = "ruby" + outputString
+        elsif type == "r"
+            rubyCommits.push(singleCommit)
+        elsif type == "h"
+            haskellCommits.push(singleCommit)
+        else
+            miscCommits.push(singleCommit)
+        end
+
+        singleCommit = ""
+        commitLine = line
         lineNumber = 0
     elsif lineNumber == 1
         if line[0...38] != "Author: Bob Webber <webber@csd.uwo.ca>"
-            puts commitLine
-            puts line
+            singleCommit += commitLine
+            singleCommit += line
         else
             lineNumber = -1
         end
+    elsif lineNumber == 4
+        singleCommit += line
+        type = line[9...10]
+        #singleCommit += type
     elsif lineNumber != -1
-        puts line
+        singleCommit += line
+        #puts line
     end
 
     if lineNumber != -1
         lineNumber += 1
     end
 end
-#puts inp
+
+puts jsCommits
+puts rubyCommits
+puts haskellCommits
