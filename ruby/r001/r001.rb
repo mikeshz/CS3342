e44b4c387b03915f567c35e30fb2230a64a50fca
Michael Song <michael.song.hz@gmail.com>
2017-01-29 17:19:08 -0500

    time r001 60 in progress

--- ruby/r001/r001.rb
+++ ruby/r001/r001.rb
@@ -1,7 +1,4 @@
-def sayHi
-   puts "Hello World!"
-end
-
+input_array = ARGV
 inp = $stdin.read
 lineNumber = 0
 commitLine = ""
@@ -18,15 +15,16 @@ inp.each_line do |line|
     #do something with line
     if lineNumber == 0 || line[0..5] == "commit"
         outputString = singleCommit + outputString
-        if type == "j"
-            jsCommits.push(singleCommit)
-            #outputString = "ruby" + outputString
-        elsif type == "r"
-            rubyCommits.push(singleCommit)
-        elsif type == "h"
-            haskellCommits.push(singleCommit)
-        else
-            miscCommits.push(singleCommit)
+        if (singleCommit != "")
+            if type == "j"
+                jsCommits.push(singleCommit)
+            elsif type == "r"
+                rubyCommits.push(singleCommit)
+            elsif type == "h"
+                haskellCommits.push(singleCommit)
+            else
+                miscCommits.push(singleCommit)
+            end
         end
 
         singleCommit = ""
@@ -34,15 +32,16 @@ inp.each_line do |line|
         lineNumber = 0
     elsif lineNumber == 1
         if line[0...38] != "Author: Bob Webber <webber@csd.uwo.ca>"
-            singleCommit += commitLine
-            singleCommit += line
+            singleCommit += commitLine[7..-1]
+            #singleCommit += line
         else
             lineNumber = -1
         end
+    elsif lineNumber == 2
+        singleCommit = line[8..-2] + singleCommit
     elsif lineNumber == 4
         singleCommit += line
         type = line[9...10]
-        #singleCommit += type
     elsif lineNumber != -1
         singleCommit += line
         #puts line
@@ -53,6 +52,17 @@ inp.each_line do |line|
     end
 end
 
-puts jsCommits
-puts rubyCommits
-puts haskellCommits
+if input_array.length == 0
+    puts jsCommits.reverse
+    puts rubyCommits.reverse
+    puts haskellCommits.reverse
+    puts miscCommits.reverse
+elsif input_array[0] == "js" || input_array[0] == "javascript"
+    puts jsCommits.reverse
+elsif input_array[0] == "ruby"
+    puts rubyCommits.reverse
+elsif input_array[0] == "haskell"
+    puts haskellCommits.reverse
+elsif input_array[0] == "other" || input_array[0] == "misc"
+    puts miscCommits.reverse
+end
